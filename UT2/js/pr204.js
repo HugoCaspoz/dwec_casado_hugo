216ce3b84082f56658c15e69315abaecbc6b1a6b
Hugo <hugobaquetas@gmail.com>
2023-09-28 08:57:00 +0200

    Ej 1-4

---
+++ UT2/js/pr204.js
@@ -0,0 +1,5 @@
+let radio = Number(prompt("Dime un radio"));
+
+let perimetro = 2*Math.PI*radio
+let area = 2 * Math.PI * radio^2
+console.log(`El perímetro del círculo de radio ${radio} es ${perimetro} y su área es ${area}`);
