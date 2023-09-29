d81b57f2821c9273eb64582a7534b9b7598ff5d5
Hugo <hugobaquetas@gmail.com>
2023-09-29 14:00:00 +0200

    Ej 4-11

--- UT2/js/pr205.js
+++ UT2/js/pr205.js
@@ -1,6 +1,10 @@
-let numero = Number(prompt("Dime un número"));
+let numero = prompt("Dime un número");
 
-while (!numero){
-    numero = Number(prompt("Dime un número"));
+while (Number.isFinite(+numero)==false || numero==null || numero.trim()=='' || numero==NaN){
+    // if (numero==0){
+    //     break;
+    // }
+    numero = prompt("Dime un número");
 }
+
 console.log(`Has introducido el número ${numero}`);
